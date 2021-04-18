b73da9fe7e142711fb7d2d37ccff1f2cee062089
Curtis Kettler <81377470+kettlerc@users.noreply.github.com>
2021-04-18 10:15:56 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+console.log("Hello from our first file!")
+
+console.log("Before conditional")
+let random = Math.random();
+if (random < 0.5) {
+    console.log("Your number is less than 0.5")
+    console.log(random)
+}
+else {
+    console.log("Your number is greater than 0.5")
+    console.log(random)
+}
+console.log("After conditional")
