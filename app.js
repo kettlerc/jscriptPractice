67167140376529046a4a3c970b8b7104b92faed9
Curtis Kettler <81377470+kettlerc@users.noreply.github.com>
2021-04-20 14:10:00 -0500

    Update app.js

--- app.js
+++ app.js
@@ -8,3 +8,14 @@ else {
     console.log("Your number is greater than 0.5")
 }
 console.log(random)
+
+const password = prompt("Please enter a new password");
+
+if (password.length >= 6) {
+    if (password.indexOf(" ") === -1) {
+        console.log("Valid password")
+    }
+}
+else {
+    console.log("Password must be at least six characters and contain no spaces.")
+}
