605253171f9a8688ba320ddd1f912970b7187fef
Curtis Kettler <81377470+kettlerc@users.noreply.github.com>
2021-04-21 11:19:03 -0500

    Update app.js

--- app.js
+++ app.js
@@ -19,3 +19,17 @@ if (password.length >= 6) {
 else {
     console.log("Password must be at least six characters and contain no spaces.")
 }
+
+const age ="34";
+if (age >=0 && age < 5 || age >= 65) {
+    console.log("FREE")
+}
+else if (age >=6 && age < 10) {
+    console.log("$10")
+}
+else if (age >= 11 && age <65) {
+    console.log("$20")
+}
+else {
+    console.log("INVALID AGE")
+}
